f4dbe96ee46804577bfa0ae87ae01c3819b4b933
Zarith G <zarith.gomez@uptc.edu.co>
2024-12-28 02:05:10 -0500

    logo

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
 );
 
-// Si quieres comenzar a medir el rendimiento de tu aplicación, pasa una función
-// para registrar los resultados (por ejemplo: reportWebVitals(console.log))
-// o envíalos a un endpoint de análisis. Más información: https://bit.ly/CRA-vitals
-reportWebVitals();
